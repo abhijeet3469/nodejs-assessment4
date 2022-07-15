f2d7f79731e036dd7870b51c6e51f9d087dc294c
Abhijeet <abhijeet3469@gmail.com>
2022-07-15 16:31:13 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,2 @@
+const users=[{email:"saikumar@gmail.com", password:"428679351"}];
+module.exports=users;
---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const auth=require('./auth')
+const app=express()
+app.use(express.json())
+app.use('/auth',auth)
+app.get('/',(req,res)=>{
+  res.send("this is main server")
+})
+app.listen(769,()=>{
+  console.log("app is running")
+})
